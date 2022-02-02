f7b452107b9afe3c1942b0d8881d313f7002aee6
Sander de Reus <sander.de.reus@oblcc.com>
2022-02-02 10:06:11 +0100

    test array

--- src/statics.ts
+++ src/statics.ts
@@ -9,4 +9,18 @@ export class statics {
   //static variables for organization id
   static readonly cab_orgId: string = 'o-lk7av3vrsd';
 
+  //static variables for vault names
+  static readonly cab_centralVaultName: string = 'cdk-cabackup-central-vault';
+  static readonly cab_memberVaultName: string = 'cdk-cabackup-member-vault';
+
+  //static variables for account id's
+  static readonly cab_backupAccount: string = '138114602286';
+  static readonly cab_memberAccount = [
+    '678826754533',
+    'b',
+    'c'
+  ];
+  
+
+
 }
